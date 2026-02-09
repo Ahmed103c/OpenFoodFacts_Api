d85aa44cdad543514704d9a1d6c54350ba11ff2c
RZEIGUI <ahmedrzeigui52@gmail.com>
2026-02-09 18:48:16 +0100

    Refactor NutritionService | Update Readme

---
+++ Nutrition/src/main/java/com/Tp2/Nutrition/Services/NutritionFactory.java
@@ -0,0 +1,41 @@
+package com.Tp2.Nutrition.Services;
+
+import org.springframework.stereotype.Component;
+
+import com.Tp2.Nutrition.Data.Dtos.NutritionScoreDto;
+import com.Tp2.Nutrition.Data.Dtos.OpenFoodFactsDto;
+import com.Tp2.Nutrition.Data.Entity.NutritionEntity;
+import com.Tp2.Nutrition.Data.Model.ResponseModel;
+
+@Component
+public class NutritionFactory {
+
+    // Entity -> ResponseModel (depuis la base de données)
+    public ResponseModel entityToResponseModel(NutritionEntity entity) {
+        ResponseModel responseModel = new ResponseModel();
+        responseModel.barCode = entity.getBarcode();
+        responseModel.name = entity.getNom();
+        responseModel.score = entity.getScoreNutrition();
+        return responseModel;
+    }
+    
+    // DTO -> ResponseModel (depuis l'API)
+    public ResponseModel dtoToResponseModel(OpenFoodFactsDto dto, String barcode) {
+        NutritionScoreDto nutritionScoreDto = new NutritionScoreDto(dto.product.nutriscoreData);
+        
+        ResponseModel responseModel = new ResponseModel();
+        responseModel.barCode = barcode;
+        responseModel.name = dto.product.name;
+        responseModel.score = nutritionScoreDto.getScore();
+        return responseModel;
+    }
+    
+    // ResponseModel -> Entity (pour sauvegarder)
+    public NutritionEntity responseModelToEntity(ResponseModel responseModel) {
+        NutritionEntity entity = new NutritionEntity();
+        entity.setBarcode(responseModel.barCode);
+        entity.setNom(responseModel.name);
+        entity.setScoreNutrition(responseModel.score);
+        return entity;
+    }
+}
--- Nutrition/src/main/java/com/Tp2/Nutrition/Services/NutritionService.java
+++ Nutrition/src/main/java/com/Tp2/Nutrition/Services/NutritionService.java
@@ -5,61 +5,44 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.Tp2.Nutrition.Data.Dtos.NutritionScoreDto;
 import com.Tp2.Nutrition.Data.Dtos.OpenFoodFactsDto;
 import com.Tp2.Nutrition.Data.Entity.NutritionEntity;
 import com.Tp2.Nutrition.Data.Model.ResponseModel;
 import com.Tp2.Nutrition.Repository.NutritionRepository;
+
+
 
 @Service
 public class NutritionService {
 
     private final RestTemplate restTemplate;
     private final NutritionRepository nutritionRepository;
+    private final NutritionFactory nutritionFactory;
 
-    public NutritionService(RestTemplate restTemplate, NutritionRepository nutritionRepository) {
+    public NutritionService(RestTemplate restTemplate, 
+                           NutritionRepository nutritionRepository,
+                           NutritionFactory nutritionFactory) {
         this.restTemplate = restTemplate;
         this.nutritionRepository = nutritionRepository;
+        this.nutritionFactory = nutritionFactory;
     }
 
-    public NutritionEntity saveNutritionData(NutritionEntity nutritionEntity) {
-        return nutritionRepository.save(nutritionEntity);
-    }
-
-    public Optional<NutritionEntity> getNutritionByBarcode(String barcode) {
-        return nutritionRepository.findByBarcode(barcode);
-    }
-    
     public ResponseModel getNutritionData(String productCode) {
-
-        if(nutritionRepository.findByBarcode(productCode).isPresent()){
-            System.out.println("Data found in database for barcode: " + productCode);  
-            NutritionEntity nutritionEntity = nutritionRepository.findByBarcode(productCode).get();
-            ResponseModel responseModel = new ResponseModel();
-            responseModel.barCode = nutritionEntity.getBarcode();
-            responseModel.name = nutritionEntity.getNom();
-            responseModel.score = nutritionEntity.getScoreNutrition();
-            return responseModel;
+        Optional<NutritionEntity> existingData = nutritionRepository.findByBarcode(productCode);
+        
+        if (existingData.isPresent()) {
+            System.out.println("Data found in database for barcode: " + productCode);
+            return nutritionFactory.entityToResponseModel(existingData.get());
         }
-        else{
-            String url = "https://world.openfoodfacts.org/api/v0/product/" + productCode + ".json";
-            OpenFoodFactsDto response = restTemplate.getForObject(url, OpenFoodFactsDto.class);
-
-            ResponseModel responseModel = new ResponseModel();
-
-            NutritionScoreDto nutritionScoreDto = new NutritionScoreDto(response.product.nutriscoreData);
-            responseModel.barCode = productCode;
-            responseModel.name = response.product.name;
-            responseModel.score = nutritionScoreDto.getScore();
-
-            NutritionEntity nutritionEntity = new NutritionEntity();
-            nutritionEntity.setBarcode(responseModel.barCode);
-            nutritionEntity.setNom(responseModel.name);
-            nutritionEntity.setScoreNutrition(responseModel.score);
-
-            nutritionRepository.save(nutritionEntity);
-
-            return responseModel;
-        }
+        
+        String url = "https://world.openfoodfacts.org/api/v0/product/" + productCode + ".json";
+        OpenFoodFactsDto dto = restTemplate.getForObject(url, OpenFoodFactsDto.class);
+        
+        ResponseModel responseModel = nutritionFactory.dtoToResponseModel(dto, productCode);
+        
+        NutritionEntity entity = nutritionFactory.responseModelToEntity(responseModel);
+        nutritionRepository.save(entity);
+        
+        return responseModel;
     }
 }
